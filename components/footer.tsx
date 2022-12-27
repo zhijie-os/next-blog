a672b480a0366ca4773428ebde90c6f7f50e75c2
zhijie.xia <zhijiexiacs@gmail.com>
2022-12-26 22:15:48 -0700

    footer hrefs

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,23 @@
 import { FaLinkedin, FaGithub } from 'react-icons/fa'
 import { AiFillMail } from 'react-icons/ai'
-// import * from '@fortawesome/free-solid-svg-icons'
-// import {} from '@fortawesome/fontawesome-svg-core'
-import Image from "next/image"
+import Link from 'next/link'
 
 export default function Footer() {
     return (
         <footer>
             <div className="mb-3 flex justify-center space-x-4" >
-
+                {/* github */}
                 <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <FaGithub size={"32px"} />
+                    <a href="https://github.com/zhijie-os" target="_blank" rel="noreferrer"><FaGithub size={"32px"} /></a>
                 </div>
-
+                {/* linkedin */}
                 <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <FaLinkedin size={"32px"} />
+                    <a href="https://www.linkedin.com/in/zhijie-xia-678b331b5/" target="_blank" rel="noreferrer"><FaLinkedin size={"32px"} /></a>
                 </div>
-
+                {/* email  */}
                 <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
-                    <AiFillMail size={"32px"} />
+                    <a href="mailto:zhijiexiacs@gmail.com"><AiFillMail size={"32px"} /></a>
                 </div>
-
-
             </div>
         </footer>
 
