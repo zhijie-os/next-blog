684935493daf347e302ca4255a73e0c3af663bf6
zhijie.xia <zhijiexiacs@gmail.com>
2022-12-26 21:57:11 -0700

    footer-icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { FaLinkedin, FaGithub } from 'react-icons/fa'
+import { AiFillMail } from 'react-icons/ai'
+// import * from '@fortawesome/free-solid-svg-icons'
+// import {} from '@fortawesome/fontawesome-svg-core'
 import Image from "next/image"
 
 export default function Footer() {
     return (
         <footer>
             <div className="mb-3 flex justify-center space-x-4" >
-            <FontAwesomeIcon icon="coffee" color="white"/>
-                {/* <FontAwesomeIcon icon="coffee" color="white"></FontAwesomeIcon> */}
 
-                <div>LinkedIn</div>
-                <div>
-                    <Image
-                        src="/mail.svg"
-                        alt="logo"
-                        width={48}
-                        height={48} />
-
+                <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaGithub size={"32px"} />
                 </div>
-                {/* <div></div> */}
+
+                <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <FaLinkedin size={"32px"} />
+                </div>
+
+                <div className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer hover:scale-110 ease-in duration-300">
+                    <AiFillMail size={"32px"} />
+                </div>
+
+
             </div>
         </footer>
 
