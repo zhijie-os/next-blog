0b6bda2604ed441318b76354f55b12c90f0836b0
notauser <zhijiexiacs@gmail.com>
2023-01-09 19:28:47 -0700

    Timeline commented

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home() {
             </p>
           </div>
 
-          <Timeline></Timeline>
+          {/* <Timeline></Timeline>  */}
         </div>
       </div>
     </Layout>
