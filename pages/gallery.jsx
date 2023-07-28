15f3b85b5f9f5615c3f78c4e797bcdef34df435d
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-07-28 15:20:01 -0400

    getStaticProps for Gallery

--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -10,159 +10,132 @@ import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import Link from "next/link";
 import ScrollTop from "../components/scrollTop";
 import LinkBar from "../components/linkbar";
 
-const photos = [
-    { src: "/photos/43.jpg", width: 2688, height: 1512 },
-    { src: "/photos/42.jpg", width: 2688, height: 1512 },
-    { src: "/photos/41.jpg", width: 2688, height: 1512 },
-    { src: "/photos/40.jpg", width: 2688, height: 1512 },
-    { src: "/photos/39.jpg", width: 2688, height: 1512 },
-    { src: "/photos/38.jpg", width: 2688, height: 1512 },
-    { src: "/photos/37.jpg", width: 4032, height: 2268 },
-    { src: "/photos/36.jpg", width: 2268, height: 4032 },
-    { src: "/photos/35.jpg", width: 1440, height: 1080 },
-    { src: "/photos/34.jpg", width: 4032, height: 3024 },
-    { src: "/photos/33.jpg", width: 2268, height: 4032 },
-    { src: "/photos/32.jpg", width: 1440, height: 1080 },
-    { src: "/photos/31.jpg", width: 4032, height: 3024 },
-    { src: "/photos/30.jpg", width: 2688, height: 1512 },
-    { src: "/photos/29.jpg", width: 1512, height: 2688 },
-    { src: "/photos/28.jpg", width: 2268, height: 4032 },
-    { src: "/photos/27.jpg", width: 4032, height: 2268 },
-    { src: "/photos/26.jpg", width: 4032, height: 2268 },
-    { src: "/photos/25.jpg", width: 4032, height: 2268 },
-    { src: "/photos/24.jpg", width: 2268, height: 4032 },
-    { src: "/photos/23.jpg", width: 4032, height: 2268 },
-    { src: "/photos/22.jpg", width: 2268, height: 4032 },
-    { src: "/photos/21.jpg", width: 4032, height: 2268 },
-    { src: "/photos/20.jpg", width: 2268, height: 4032 },
-    { src: "/photos/19.jpg", width: 4032, height: 2268 },
-    { src: "/photos/18.jpg", width: 2268, height: 4032 },
-    { src: "/photos/17.jpg", width: 4032, height: 2268 },
-    { src: "/photos/16.jpg", width: 4032, height: 2268 },
-    { src: "/photos/15.jpg", width: 4032, height: 2268 },
-    { src: "/photos/14.jpg", width: 2268, height: 4032 },
-    { src: "/photos/13.jpg", width: 1080, height: 1440 },
-    { src: "/photos/12.jpeg", width: 4032, height: 3024 },
-    { src: "/photos/11.jpeg", width: 4032, height: 3024 },
-    { src: "/photos/10.jpeg", width: 4032, height: 3024 },
-    { src: "/photos/9.jpeg", width: 4032, height: 3024 },
-    { src: "/photos/8.jpg", width: 1440, height: 1080 },
-    { src: "/photos/7.jpg", width: 1080, height: 1440 },
-    { src: "/photos/6.jpg", width: 1440, height: 1080 },
-    { src: "/photos/5.jpg", width: 1080, height: 1440 },
-    { src: "/photos/4.jpg", width: 1440, height: 1080 },
-    { src: "/photos/3.jpg", width: 750, height: 745 },
-    { src: "/photos/2.webp", width: 1600, height: 2133 },
-    { src: "/photos/1.jpg", width: 1080, height: 1440 },
-]
-
+import fs from "fs";
+import path from "path";
+const sizeOf = require("image-size");
 
 function NextJsImage({
-    photo,
-    imageProps: { alt, title, sizes, className, onClick },
-    wrapperStyle,
+  photo,
+  imageProps: { alt, title, sizes, className, onClick },
+  wrapperStyle,
 }) {
-    return (
-        <div style={{ ...wrapperStyle, position: "relative" }}>
-            <Image
-                // fill
-                src={photo}
-                placeholder={"blurDataURL" in photo ? "blur" : undefined}
-                {...{ alt, title, sizes, className, onClick }}
-            />
-        </div>
-    );
+  return (
+    <div style={{ ...wrapperStyle, position: "relative" }}>
+      <Image
+        // fill
+        src={photo}
+        placeholder={"blurDataURL" in photo ? "blur" : undefined}
+        {...{ alt, title, sizes, className, onClick }}
+      />
+    </div>
+  );
 }
 
+export default function ImageGallery({ photos }) {
+  const [index, setIndex] = useState(-1);
+  const home = false;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <header className="flex justify-center mb-4">
+        {home ? (
+          <>
+            <div className="flex flex-col">
+              <div className="flex justify-center m-1">
+                <Image
+                  priority
+                  src="/images/avatar.jpg"
+                  className="rounded-full"
+                  height={144}
+                  width={144}
+                  alt=""
+                ></Image>
+              </div>
+              <div className="flex ml-2">
+                “That which we need the most will be found where we least want
+                to look.” - Carl Jung.
+              </div>
 
-export default function ImageGallery() {
-    const [index, setIndex] = useState(-1)
-    const home = false
-
-    return (
-        <div >
-            <Navbar></Navbar>
-            <header className="flex justify-center mb-4">
-                {home ? (
-                    <>
-                        <div className="flex flex-col">
-                            <div className="flex justify-center m-1">
-                                <Image
-                                    priority
-                                    src="/images/avatar.jpg"
-                                    className="rounded-full"
-                                    height={144}
-                                    width={144}
-                                    alt=""
-                                ></Image>
-                            </div>
-                            <div className="flex ml-2">“That which we need the most will be found where we least want to look.” -  Carl Jung.</div>
-
-                            <LinkBar></LinkBar>
-                        </div>
-
-                    </>
-                ) : (
-                    <>
-                        <div className="flex flex-col">
-                            <div className="flex justify-center m-1">
-                                <Link href="/">
-                                    <Image
-                                        priority
-                                        className="rounded-full"
-                                        src="/images/avatar.png"
-                                        height={88}
-                                        width={88}
-                                        alt=""
-                                    />
-                                </Link>
-                            </div>
-                            <div className="flex ml-2">“That which we need the most will be found where we least want to look.” -  Carl Jung.</div>
-                        </div>
-                    </>
-                )
-                }
-            </header>
-
-            <PhotoAlbum
-                className=""
-                layout="rows"
-                photos={photos}
-                renderPhoto={NextJsImage}
-                sizes={{
-                    size: "calc(100vw - 40px)",
-                    sizes: [
-                        { viewport: "(max-width: 299px)", size: "calc(100vw - 10px)" },
-                        { viewport: "(max-width: 599px)", size: "calc(100vw - 20px)" },
-                        { viewport: "(max-width: 1199px)", size: "calc(100vw - 30px)" },
-                    ],
-                }}
-                onClick={({ index }) => setIndex(index)}
-            />
-
-            <Lightbox
-                slides={photos}
-                open={index >= 0}
-                index={index}
-                close={() => setIndex(-1)}
-                // enable optional lightbox plugins
-                plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
-            />
-
-
-            <ScrollTop></ScrollTop>
-            <div className="mt-4">
-                <Footer></Footer>
+              <LinkBar></LinkBar>
             </div>
-        </div>
-    );
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col">
+              <div className="flex justify-center m-1">
+                <Link href="/">
+                  <Image
+                    priority
+                    className="rounded-full"
+                    src="/images/avatar.png"
+                    height={88}
+                    width={88}
+                    alt=""
+                  />
+                </Link>
+              </div>
+              <div className="flex ml-2">
+                “That which we need the most will be found where we least want
+                to look.” - Carl Jung.
+              </div>
+            </div>
+          </>
+        )}
+      </header>
+
+      <PhotoAlbum
+        className=""
+        layout="rows"
+        photos={photos}
+        renderPhoto={NextJsImage}
+        sizes={{
+          size: "calc(100vw - 40px)",
+          sizes: [
+            { viewport: "(max-width: 299px)", size: "calc(100vw - 10px)" },
+            { viewport: "(max-width: 599px)", size: "calc(100vw - 20px)" },
+            { viewport: "(max-width: 1199px)", size: "calc(100vw - 30px)" },
+          ],
+        }}
+        onClick={({ index }) => setIndex(index)}
+      />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        // enable optional lightbox plugins
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+
+      <ScrollTop></ScrollTop>
+      <div className="mt-4">
+        <Footer></Footer>
+      </div>
+    </div>
+  );
 }
 
+export async function getStaticProps() {
+  const photoDirectory = path.join(process.cwd(), "public/photos");
+  const photoFiles = fs.readdirSync(photoDirectory);
 
+  const photos = photoFiles.map((photo) => {
+    const filePath = `/photos/${photo}`;
+    const dimensions = sizeOf("public" + filePath);
+    return {
+      src: filePath,
+      width: dimensions.width,
+      height: dimensions.height,
+    };
+  });
+
+  return { props: { photos } };
+}
