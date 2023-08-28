1df700d564808bd835844a0d6699a13df5e78b82
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-08-28 11:25:19 -0400

    blog reverted

---
+++ pages/posts.tsx
@@ -0,0 +1,44 @@
+import { getSortedPostsData } from "../lib/posts"
+import Link from "next/link"
+import Date from "../components/date"
+import Layout from "../components/layout"
+import Head from "next/head"
+
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Posts({ allPostsData }: { allPostsData: any }) {
+    return (
+        <Layout home={false}>
+            <Head>
+                <title>Blog Posts | Zhijie Xia</title>
+            </Head>
+
+            <div className="max-w-4xl flex flex-col pb-8">
+                <div className="space-y-2  md:space-y-5 divide-y divide-gray-700 ">
+                    <h1 className="m-4 text-3xl font-extrabold leading- tracking-tight text-gray-900 dark:text-gray-100 sm:leading-10 md:text-4xl md:leading-14">
+                        Blog
+                    </h1>
+                    <div className="m-4 flex flex-col divide-y divide-gray-700">
+                        {allPostsData.map(({ id, date, title }: { id: string, date: string, title: string }) => (
+                            <li className="m-2 text-xl" key={id}>
+                                <Link href={`/posts/${id}`}>{title}</Link>
+                                <br />
+                                <small >
+                                    <Date dateString={date} />
+                                </small>
+                            </li>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </Layout>
+
+    )
+}
---
+++ posts/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: 'Hello World: new dev blog in NextJS'
+date: '2022-12-29'
+---
+# Hello World!
+
+Hello and welcome to my newly redesigned personal developer blog! It was built using NextJS, and I decided to switch from the MERN stack in order to improve SEO and get better search engine rankings. As a programmer and computer science student, I want to use this blog to share my experiences and knowledge on various technical topics.
+
+Thank you for visiting and joining me on this journey. I look forward to sharing my thoughts and insights on the world of programming with you. Stay tuned for more updates as the blog officially gets underway!
