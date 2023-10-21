2996a56dd3c8dfa5ee02ad93f013d9dc1685e09a
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-10-21 00:32:30 -0700

    grid 2-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,66 +47,72 @@ export default function Home() {
         <div className="max-w-4xl w-full mx-auto p-2 flex flex-col justify-center items-center">
           <div className="flex flex-col mx-auto">
             <Image
-              className="rounded-md pb-20]"
+              className="rounded-md pb-10"
               src="/images/86.gif"
               width={size.width}
               height={size.height}
               alt="eight_six"
             ></Image>
-            {/* introduction section  */}
-            <h1 className="text-4xl py-2 font-bold">
-              <span className="text-cyan-500">Zhijie Xia</span> is an
-              undergraduate student, researcher at{" "}
-              <Link className="underline" href={"https://www.ucalgary.ca/"}>
-                University of Calgary
-              </Link>
-              , and firmware developer at{" "}
-              <Link className="underline" href="https://thinklucid.com/">
-                Lucid Vision Labs
-              </Link>
-              .
-            </h1>
+            <div className="grid grid-cols-3 gap-4">
+              {/* introduction section  */}
+              <div className="col-span-2">
+                <h1 className="text-4xl py-2 font-bold">
+                  <span className="text-cyan-500">Zhijie Xia</span> is an
+                  undergraduate student, researcher at{" "}
+                  <Link className="underline" href={"https://www.ucalgary.ca/"}>
+                    University of Calgary
+                  </Link>
+                  , and firmware developer at{" "}
+                  <Link className="underline" href="https://thinklucid.com/">
+                    Lucid Vision Labs
+                  </Link>
+                  .
+                </h1>
 
-            {/* student section  */}
-            <p className="py-4 text-center">
-              As an exceptional Computer Science major at the University of
-              Calgary, Zhijie has received multiple scholarships and awards for
-              his outstanding academic performance. He has explored a diverse
-              range of essential courses, such as Operating Systems, Algorithm &
-              Data Structure, Compiler Construction, Computer Networks, Network
-              Security, and System Security. Eager to apply his well-honed
-              skills in software engineering, he seeks exciting opportunities to
-              contribute to FOSS projects.
-            </p>
+                {/* student section  */}
+                <p className="py-4 text-center">
+                  As an exceptional Computer Science major at the University of
+                  Calgary, Zhijie has received multiple scholarships and awards
+                  for his outstanding academic performance. He has explored a
+                  diverse range of essential courses, such as Operating Systems,
+                  Algorithm & Data Structure, Compiler Construction, Computer
+                  Networks, Network Security, and System Security. Eager to
+                  apply his well-honed skills in software engineering, he seeks
+                  exciting opportunities to contribute to FOSS projects.
+                </p>
 
-            {/* research section */}
-            <p className="py-4 text-center">
-              In the realm of research, Zhijie focuses on two compelling areas:
-              System Security and Mixed Reality. Under the expert guidance of
-              Dr. Joel Reardon and Dr. Ryo Suzuki, respectively, he has made
-              significant strides in his investigations. One notable achievement
-              is his co-authorship of the paper{" "}
-              <Link
-                className="underline"
-                href="https://drive.google.com/file/d/1A1r61O4RwECgX0xsq2rJ8P3FBne6VXTL/view?usp=drive_link"
-              >
-                RealityCanvas
-              </Link>
-              , presented at the prestigious UIST 2023 conference.
-            </p>
+                {/* research section */}
+                <p className="py-4 text-center">
+                  In the realm of research, Zhijie focuses on two compelling
+                  areas: System Security and Mixed Reality. Under the expert
+                  guidance of Dr. Joel Reardon and Dr. Ryo Suzuki, respectively,
+                  he has made significant strides in his investigations. One
+                  notable achievement is his co-authorship of the paper{" "}
+                  <Link
+                    className="underline"
+                    href="https://drive.google.com/file/d/1A1r61O4RwECgX0xsq2rJ8P3FBne6VXTL/view?usp=drive_link"
+                  >
+                    RealityCanvas
+                  </Link>
+                  , presented at the prestigious UIST 2023 conference.
+                </p>
 
-            {/* developer section */}
-            <p className="py-4 pb-6 text-center">
-              Having embarked on a career as a software engineer, Zhijie has
-              already gained valuable experience at a dynamic startup company
-              based in Toronto. This opportunity allowed him to collaborate with
-              a myriad of interesting and competent professionals, providing him
-              with an invaluable taste of the tech industry&rsquo;s vibrancy and
-              innovation.
-            </p>
+                {/* developer section */}
+                <p className="py-4 pb-6 text-center">
+                  Having embarked on a career as a software engineer, Zhijie has
+                  already gained valuable experience at a dynamic startup
+                  company based in Toronto. This opportunity allowed him to
+                  collaborate with a myriad of interesting and competent
+                  professionals, providing him with an invaluable taste of the
+                  tech industry&rsquo;s vibrancy and innovation.
+                </p>
+              </div>
+              {/* timeline section */}{" "}
+              <div className="col-span-1">
+                <Timeline></Timeline>
+              </div>
+            </div>
           </div>
-
-          <Timeline></Timeline>
         </div>
       </div>
     </Layout>
