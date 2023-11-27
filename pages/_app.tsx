1863a8613a48fac9e4f6baa863787111433b39c3
Zhijie Xia <76631942+zhijie-os@users.noreply.github.com>
2023-11-27 15:27:14 +0800

    crapyy implementation of the reality canvas paper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Ubuntu } from "@next/font/google";
-import { Worker } from "@react-pdf-viewer/core";
 
 const ubuntu = Ubuntu({
   weight: "400",
@@ -10,10 +9,8 @@ const ubuntu = Ubuntu({
 // use to configure global.css, only in this file
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-      <main className={ubuntu.className}>
-        <Component {...pageProps} />
-      </main>
-    </Worker>
+    <main className={ubuntu.className}>
+      <Component {...pageProps} />
+    </main>
   );
 }
--- pages/publication.tsx
+++ pages/publication.tsx
@@ -1,37 +1,51 @@
 import Layout from "../components/layout";
 import Head from "next/head";
-// Import the main component
-import { Viewer } from "@react-pdf-viewer/core";
-// Import the styles
-// import "@react-pdf-viewer/core/lib/styles/index.css";
+import Image from "next/image";
+import { Worker } from "@react-pdf-viewer/core";
+import Modal from "../components/modal";
+import { useEffect, useState } from "react";
+
+
+function Hero() {
+  const [showModal, setShowModal] = useState(false);
+
+  const updateShowModal = () => {
+    setShowModal(!showModal);
+  }
+  
+  return (
+    <div
+      className="flex flex-col max-w-sm rounded-md bg-white p-5 text-slate-600 hover:shadow-lg hover:cursor-pointer"
+      onClick={() => setShowModal(true)}
+    >
+      <div className="text-slate-900">
+        RealityCanvas: Augmented Reality Sketching for Embedded and Responsive
+        Scribble Animation Effects
+      </div>
+      <div className="text-slate-900">
+        <small>UIST 2023</small>
+      </div>
+      <Image
+        className="rounded object-cover object-center md:h-36 lg:h-48"
+        src={"/teaser/reality-canvas.gif"}
+        alt={"RealityCanvas"}
+        width={400}
+        height={400}
+      />
+      <Modal showModal={showModal} updateShowModal={updateShowModal} />
+    </div>
+  );
+}
 
 export default function Publication() {
+
+
   return (
     <Layout home={false}>
       <Head>
         <title>Publication | Zhijie Xia</title>
       </Head>
-{/* 
-      <div className="max-w-4xl flex flex-col divide-y divide-gray-200 dark:divide-gray-700">
-        <div>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Besides being a developer, I am also a researcher at Programmable
-            Reality Lab
-          </p>
-        </div>
-      </div> */}
-
-      <div
-      // className="flex justify-center"
-        style={{
-          // position: "absolute",
-          // border: "1px solid rgba(0, 0, 0, 0.3)",
-          width: "1200px",
-          // height: "750px",
-        }}
-      >
-        <Viewer fileUrl="/reality-canvas.pdf" />
-      </div>
+      <Hero></Hero>
     </Layout>
   );
 }
